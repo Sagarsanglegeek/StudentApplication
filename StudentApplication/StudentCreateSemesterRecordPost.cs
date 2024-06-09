c9955dc4bbc74abf3ba2b10f6f69f19f4ad826a6
seema <seema@SEEMA_SANGLE>
2024-06-09 22:50:37 +0530

    Add project files.

---
+++ StudentApplication/StudentCreateSemesterRecordPost.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApplication
+{
+    public class StudentCreateSemesterRecordPost : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            //Plugin context
+            IPluginExecutionContext context=(IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            ITracingService tracingService=(ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            try
+            {
+                // D365 service object
+                IOrganizationServiceFactory organizationServiceFactory=(IOrganizationServiceFactory)serviceProvider.GetService (typeof(IOrganizationServiceFactory));
+                IOrganizationService service = organizationServiceFactory.CreateOrganizationService(context.UserId);
+
+                int sem=0;
+
+                if(context.MessageName.ToString() !="create" && context.Stage !=40 )
+                {
+                    return;
+                }
+
+                if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+                {
+                    Entity entity = (Entity)context.InputParameters["Target"];
+
+                    //string fetchXml = @"<fetch>
+                    //                    <entity name='ss_student'>
+                    //                    <attribute name='ss_studentid'/>
+                    //                    <attribute name='ss_name'/>
+                    //                    <attribute name='ss_course'/>
+                    //                    <filter type='and'>
+                    //                    <condition attribute='statecode' operator='eq' value='0'/>
+                    //                    <condition attribute='ss_studentid' operator='eq' value='"+entity.Id+@"'/>
+                    //                    </filter>
+                    //                    <link-entity name='ss_course' from='ss_courseid' to='ss_course' link-type='inner' alias='cou'>
+                    //                    <attribute name='ss_semester'/>
+                    //                    <filter type='and'>
+                    //                    <condition attribute='ss_semester' operator='not-null'/>
+                    //                    </filter>
+                    //                    </link-entity>
+                    //                    </entity>
+                    //                    </fetch>";
+
+                    Guid courseGuid = Guid.Empty;//= ((Guid)(entity.GetAttributeValue<Guid>("")));
+
+                    if(entity.Attributes.Contains("ss_course") && entity.Attributes["ss_course"] !=null)
+                    {
+                        courseGuid = ((EntityReference)(entity.Attributes["ss_course"])).Id;
+                    }
+
+
+                    string fetchXml = @"<fetch version='1.0'>
+                                        <entity name='ss_course'>
+                                        <attribute name='ss_courseid'/>
+                                        <attribute name='ss_semester'/>
+                                        <filter type='and'>
+                                        <condition attribute='ss_courseid' operator='eq' value='"+ courseGuid +@"'/>
+                                        </filter>
+                                        </entity>
+                                        </fetch>";
+
+                    EntityCollection collection = service.RetrieveMultiple(new FetchExpression(fetchXml));
+
+                    foreach(Entity entityRecord in collection.Entities)
+                    {
+                        if(entityRecord.Attributes.Contains("ss_semester") && entityRecord.Attributes["ss_semester"] !=null)
+                        {
+                            sem = (int)entityRecord.Attributes["ss_semester"];
+                        }
+                        break;
+                    }
+
+                    for(int i = 0; i<sem; i++)
+                    {
+                        Entity semRecord = new Entity("ss_semester");
+                        semRecord["ss_name"] = "Sem" + i;
+                        service.Create(semRecord);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new InvalidPluginExecutionException();
+            
+            }
+
+        }
+    }
+}
---
+++ StudentApplication/Student_CreateAutoNumberPre.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApplication
+{
+    public class Student_CreateAutoNumberPre : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            
+            IPluginExecutionContext executionContext=(IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            ITracingService tracingService=(ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            try
+            {
+                IOrganizationServiceFactory organizationServiceFactory=(IOrganizationServiceFactory)serviceProvider.GetService (typeof(IOrganizationServiceFactory));
+                IOrganizationService service = (IOrganizationService)organizationServiceFactory.CreateOrganizationService(executionContext.UserId);
+
+                Entity studentEntity = (Entity)executionContext.InputParameters["Target"];
+                if(executionContext.MessageName.ToLower()!="create")
+                {
+                   return;
+                }
+
+                string autoNumber = string.Empty, prefix = string.Empty, suffix = string.Empty, seprator = string.Empty, currentnumber = string.Empty,
+                year = string.Empty, month = string.Empty, day = string.Empty;
+
+                DateTime today= DateTime.Now;
+                year = today.Year.ToString();
+                month = today.Month.ToString("00");  //06
+                day = today.Day.ToString("00");
+
+                QueryExpression queryExpression = new QueryExpression()
+                {
+                    EntityName = "ss_studentnumberconfig",
+                    ColumnSet = new ColumnSet("ss_name", "ss_prefix", "ss_separator", "ss_suffix", "ss_currentnumber")
+                };
+
+                queryExpression.Criteria.AddCondition("ss_name", ConditionOperator.Equal, "studentautonumber");
+
+               EntityCollection entityCollection= service.RetrieveMultiple(queryExpression);
+
+               if(entityCollection.Entities.Count>0 )
+                {
+                    tracingService.Trace("Student config record count" + entityCollection.Entities.Count);
+
+                    Entity studentAutonumber= entityCollection.Entities[0];
+                    prefix = studentAutonumber["ss_prefix"].ToString();
+                    suffix = studentAutonumber["ss_suffix"].ToString() ;
+                    seprator = studentAutonumber["ss_separator"].ToString();
+                    currentnumber = studentAutonumber["ss_currentnumber"].ToString();
+                    tracingService.Trace("Current record number on Config Entity:" + currentnumber);
+                    int tempcurrentNumber = int.Parse(currentnumber);
+                    tempcurrentNumber++;  //0000000 //000001
+                    currentnumber=tempcurrentNumber.ToString("000000");
+
+                    autoNumber = prefix + seprator + year + month + day + seprator + suffix + seprator + currentnumber;
+                    tracingService.Trace("AutoNumber:" + autoNumber);
+
+                   
+
+
+                    QueryExpression qeStudent = new QueryExpression()
+                    {
+                        EntityName = "ss_student",
+                        ColumnSet = new ColumnSet("ss_studentnumber")
+
+                    };
+
+                    qeStudent.Criteria.AddCondition("ss_studentnumber", ConditionOperator.Equal, autoNumber);
+                    EntityCollection studentRecord=service.RetrieveMultiple(qeStudent);
+                    if(studentRecord.Entities.Count>0 )
+                    {
+                        throw new Exception("Duplicate Student found with Student ID:" + autoNumber);
+                    }
+
+                    // Student number
+                    //Entity entity1 = new Entity("ss_student");
+                    //entity1.Id = studentEntity.Id;
+                    //entity1["ss_studentnumber"] = autoNumber.ToString();
+                    //service.Update(entity1);
+
+                    //The below code will update student config number
+                    studentEntity.Attributes["ss_studentnumber"] = autoNumber.ToString();
+                    Entity entity = new Entity("ss_studentnumberconfig");
+                    entity.Id = studentAutonumber.Id;
+                    entity["ss_currentnumber"] = currentnumber;
+                    service.Update(entity);
+
+                    //Entity entity = new Entity("ss_student");
+                    //entity.Id = executionContext.PrimaryEntityId;
+                    //entity["ss_studentnumber"] = autoNumber;
+                    //service.Update(entity);
+
+
+
+
+
+
+                }
+
+            }
+            catch(Exception ex)
+            {
+                throw new InvalidPluginExecutionException(ex.Message);
+            }
+
+
+            
+        }
+    }
+}
