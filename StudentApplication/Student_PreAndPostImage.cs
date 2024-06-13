f6b3e673611d6345aaf16ef89e3271da9bd4bac2
Sagarsanglegeek <38125961+Sagarsanglegeek@users.noreply.github.com>
2024-06-13 10:25:10 +0530

    Pre And post Image Plugin

--- StudentApplication/Student_PreAndPostImage.cs
+++ StudentApplication/Student_PreAndPostImage.cs
@@ -7,64 +7,67 @@ using System.Threading.Tasks;
 
 namespace StudentApplication
 {
-    public class Student_UpdateGetAndSet:IPlugin
+    public class Student_PreAndPostImage : IPlugin
     {
 
         public void Execute(IServiceProvider serviceProvider)
         {
             try
             {
-                string  firstName = string.Empty, lastName = string.Empty,email=string.Empty;
-                int gender=0, title=0;
+                string firstName = string.Empty, lastName = string.Empty, email = string.Empty;
+                int gender = 0, title = 0;
                 Guid courseID = Guid.Empty;
-                decimal coursefees=0;
+                decimal coursefees = 0;
                 IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
                 ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
 
-                if(context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
+                if (context.InputParameters.Contains("Target") && context.InputParameters["Target"] is Entity)
                 {
                     ITracingService trace = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
                     IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
                     IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
 
-                    if(context.MessageName.ToLower()=="create" && context.Stage ==40)
+                    if (context.MessageName.ToLower() == "update" && context.Stage == 10)
                     {
                         Entity entity = (Entity)context.InputParameters["Target"];
+                        Entity preImage = (Entity)context.PreEntityImages["PreImage"];
+                        Entity postImage = (Entity)context.PostEntityImages["PostImage"];
+
 
                         //Get
-                        if(entity.Attributes.Contains("ss_title") && entity.Attributes["ss_title"]!=null)
+                        if (preImage.Attributes.Contains("ss_title") && preImage.Attributes["ss_title"] != null)
                         {
-                            title =((OptionSetValue) (entity.Attributes["ss_title"])).Value;
+                            title = ((OptionSetValue)(preImage.Attributes["ss_title"])).Value;
                             trace.Trace("title:" + title);
                         }
-                        if (entity.Attributes.Contains("ss_name") && entity.Attributes["ss_name"] != null)
+                        if (preImage.Attributes.Contains("ss_name") && preImage.Attributes["ss_name"] != null)
                         {
-                            firstName = entity.Attributes["ss_name"].ToString();
+                            firstName = preImage.Attributes["ss_name"].ToString();
                             trace.Trace("firstName:" + firstName);
                         }
-                        if (entity.Attributes.Contains("ss_lastname") && entity.Attributes["ss_lastname"] != null)
+                        if (preImage.Attributes.Contains("ss_lastname") && preImage.Attributes["ss_lastname"] != null)
                         {
-                            lastName = entity.Attributes["ss_lastname"].ToString();
+                            lastName = preImage.Attributes["ss_lastname"].ToString();
                             trace.Trace("lastName:" + lastName);
                         }
-                        if (entity.Attributes.Contains("ss_gender") && entity.Attributes["ss_gender"] != null)
+                        if (preImage.Attributes.Contains("ss_gender") && preImage.Attributes["ss_gender"] != null)
                         {
-                            gender = ((OptionSetValue)entity.Attributes["ss_gender"]).Value;
+                            gender = ((OptionSetValue)preImage.Attributes["ss_gender"]).Value;
                             trace.Trace("gender:" + gender);
                         }
-                        if (entity.Attributes.Contains("ss_email") && entity.Attributes["ss_email"] != null)
+                        if (preImage.Attributes.Contains("ss_email") && preImage.Attributes["ss_email"] != null)
                         {
-                            email = entity.Attributes["ss_email"].ToString(); ;
+                            email = preImage.Attributes["ss_email"].ToString(); ;
                             trace.Trace("email:" + email);
                         }
-                        if (entity.Attributes.Contains("ss_course") && entity.Attributes["ss_course"] != null)
+                        if (preImage.Attributes.Contains("ss_course") && preImage.Attributes["ss_course"] != null)
                         {
-                            courseID = ((EntityReference)(entity.Attributes["ss_course"])).Id;
+                            courseID = ((EntityReference)(preImage.Attributes["ss_course"])).Id;
                             trace.Trace("courseID:" + courseID);
                         }
-                        if (entity.Attributes.Contains("ss_coursefees") && entity.Attributes["ss_coursefees"] != null)
+                        if (preImage.Attributes.Contains("ss_coursefees") && preImage.Attributes["ss_coursefees"] != null)
                         {
-                            coursefees = ((Money)(entity.Attributes["ss_coursefees"])).Value;
+                            coursefees = (decimal)(preImage.Attributes["ss_coursefees"]);
                             trace.Trace("coursefees:" + coursefees);
                         }
 
@@ -72,24 +75,25 @@ namespace StudentApplication
 
                         Entity studentEntity = new Entity("ss_student");
                         studentEntity.Attributes["ss_title"] = new OptionSetValue(title);
-                        studentEntity.Attributes["ss_name"] = firstName+DateTime.Now.Date;
+                        studentEntity.Attributes["ss_name"] = firstName;
                         studentEntity.Attributes["ss_lastname"] = lastName;
                         studentEntity.Attributes["ss_gender"] = new OptionSetValue(gender);
-                        studentEntity.Attributes["ss_email"]=email;
+                        studentEntity.Attributes["ss_email"] = email;
                         studentEntity.Attributes["ss_course"] = new EntityReference("ss_course", courseID);
                         studentEntity.Attributes["ss_coursefees"] = new Money(coursefees);
-                        Guid recordGuid=service.Create(studentEntity);
-                        tracingService.Trace("Record Guid:" + recordGuid);
+                        service.Create(studentEntity);
                     }
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 throw new Exception(ex.Message);
 
             }
         }
 
-       
+
     }
+
 }
+
